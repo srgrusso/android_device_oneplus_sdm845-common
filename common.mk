3169357d109d5b81c71277dfa250311ca5ac3cb4
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-22 17:50:43 +0530

    sdm845-common: Introduce DeviceSettings
    - Imported from https://github.com/AOSiP-Devices/device_oneplus_sdm845-common/tree/f55c0218951d9b0f6586bdf5041a64ea14c96460/DeviceSettings
    - Adapted to AndroidX
    - Includes all translations from pie cycle
    - Dropped DC-Dimming QS Tile
    
    Change-Id: I7dd192534830ad927eafe83e77a9f7954095c75a
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- common.mk
+++ common.mk
@@ -98,6 +98,10 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
@@ -177,7 +181,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Trust HAL
