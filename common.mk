5d1c44a8fc49fd71a26d4de301054e4eb36297d5
Jackeagle <jackeagle102@gmail.com>
2019-12-04 09:47:44 +0100

    sdm845-common: Build NoCutoutOverlay
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- common.mk
+++ common.mk
@@ -150,6 +150,10 @@ PRODUCT_PACKAGES += \
     vendor.nxp.nxpese@1.0:64 \
     vendor.nxp.nxpnfc@1.0:64
 
+# Hide notch
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Power
 PRODUCT_PACKAGES += \
     power.qcom:64
