fe60011dd89728fa94a528c7b4d1c889bb55f68a
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-11-04 15:04:40 +0500

    feat(Charging): Use Pixel Offline Charging animation
    * from redfin-user RD1A.201105.003.A1-6886512
    
    Change-Id: I25e5b0e8cb6e463fb273227482461692ab8b6272
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- common.mk
+++ common.mk
@@ -417,6 +417,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.qcom:64
     
+# Pixel Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
