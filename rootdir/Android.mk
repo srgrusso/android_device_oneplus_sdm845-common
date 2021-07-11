58ff975a83e08701df30fdd4ead35b0849c38297
srgrusso <srgrusso240@gmail.com>
2021-07-11 15:53:44 -0500

    Revert "sdm845-common: Enable Adaptive Charging"
    This reverts commit 57b3d16ed2f7b54360ed441b563ad5e0f113494f.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,14 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.adaptive_charging.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.adaptive_charging.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.device.extras.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
