0f25fa6ea783b21ebb446122f78fc6d9f2dec9d5
srgrusso <srgrusso240@gmail.com>
2021-09-11 11:30:45 -0500

    sdm845-common: Use sunfish September fp

--- init/init_sdm845.cpp
+++ init/init_sdm845.cpp
@@ -85,4 +85,8 @@ void vendor_load_properties()
 {
 	// Load dalvik config
 	load_dalvikvm_properties();
+
+    // fingerprint
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RQ3A.210905.001/7511028:user/release-keys");
 }
+
