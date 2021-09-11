a652524770006d4aa1d83670c29b705662d5d166
srgrusso <srgrusso240@gmail.com>
2021-09-11 11:14:15 -0500

    Blissify

--- common.mk
+++ common.mk
@@ -29,7 +29,7 @@ $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-bliss
 
 PRODUCT_PACKAGES += \
     OdmOverlay-OPSystemUI \
