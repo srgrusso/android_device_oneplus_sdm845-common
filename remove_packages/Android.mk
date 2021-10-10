76dadd519ef158151db7f336d44d51680faee1bc
srgrusso <srgrusso240@gmail.com>
2021-10-10 13:54:49 -0500

    sdm845-common: Exclude Phonograph and Chrome

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CalendarGooglePrebuilt DiagnosticsToolPrebuilt Maps Photos PixelLiveWallpaperPrebuilt RecorderPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase SprintDM SprintHM WallpapersBReel2020
+LOCAL_OVERRIDES_PACKAGES := CalendarGooglePrebuilt DiagnosticsToolPrebuilt Maps Photos PixelLiveWallpaperPrebuilt RecorderPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase SprintDM SprintHM WallpapersBReel2020 Phonograph Chrome Chrome-Stub
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
