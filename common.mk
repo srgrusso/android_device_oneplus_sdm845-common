dfcee758e1acaf70f604097c3de764e0dfbe8158
SevralT <a.chycherin@outlook.com>
2021-11-19 07:36:43 +0000

    feat(Hardware): Import back stuff from hardware/oneplus

--- common.mk
+++ common.mk
@@ -399,10 +399,10 @@ PRODUCT_PACKAGES += \
 
 # OnePlus
 PRODUCT_PACKAGES += \
-    oneplus-fwk
+    oneplus-fwk.oneplus_sdm845
 
-#PRODUCT_BOOT_JARS += \
-#    oneplus-fwk
+PRODUCT_BOOT_JARS += \
+    oneplus-fwk.oneplus_sdm845
     
 # OnePlus Camera HIDL
 PRODUCT_PACKAGES += \
@@ -505,7 +505,6 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/oneplus
 
 # Telephony
 PRODUCT_PACKAGES += \
