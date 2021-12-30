f845465e5de21f0341679d74d4cce84cf1ffcc3a
SevralT <a.chycherin@outlook.com>
2021-12-30 09:23:47 +0000

    feat(Libperfmgr): Import it from SyberiaOS Tree Link: https://github.com/SyberiaProject-Devices/platform_device_oneplus_oneplus6
    All rights reserved by their authors!

--- common.mk
+++ common.mk
@@ -404,9 +404,13 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.qcom:64 \
+    android.hardware.power-service.op6-libperfmgr \
+    android.hardware.power.stats@1.0-service.op6 \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/lib64/pixel-power-ext-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib64/pixel-power-ext-V1-ndk_platform.so
 
 # Pixel Offline charger
 PRODUCT_PACKAGES += \
@@ -500,7 +504,9 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/oneplus
+    hardware/oneplus \
+    hardware/google/pixel \
+    hardware/google/interface
 
 # Telephony
 PRODUCT_PACKAGES += \
